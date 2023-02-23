b5ac309cccb59f1cd153324e51130e44aef60ec1
Gustavo Siqueira <guss.ns@gmail.com>
2023-02-23 10:39:52 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,5 +19,10 @@ public class Program {
     List<Seller> list = sellerDao.findByDepartment(department);
     for (Seller obj : list)
       System.out.println(obj);
+
+    System.out.println("\n==== TEST 3: seller findAll ==== ");
+    list = sellerDao.findAll();
+    for (Seller obj : list)
+      System.out.println(obj);
   }
 }
